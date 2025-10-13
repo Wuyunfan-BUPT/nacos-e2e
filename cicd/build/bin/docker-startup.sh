20a4876b508fa8d3e36413a7ad65600210c842e5
Wuyunfan-BUPT <1564396093@qq.com>
2025-10-13 16:06:24 +0800

    fix: 更新dockerfile以支持JDK17
    Change-Id: I151337cf15e6edb01354dc4aa0bc81bfda41c722

--- cicd/build/bin/docker-startup.sh
+++ cicd/build/bin/docker-startup.sh
@@ -14,7 +14,7 @@
 set -x
 export CUSTOM_SEARCH_NAMES="application"
 export CUSTOM_SEARCH_LOCATIONS=file:${BASE_DIR}/conf/
-export MEMBER_LIST=""
+export MEMBER_LIST="$MEMBER_LIST"
 PLUGINS_DIR="/home/nacos/plugins/peer-finder"
 function print_servers() {
    if [[ ! -d "${PLUGINS_DIR}" ]]; then
@@ -27,20 +27,34 @@ function print_servers() {
     sleep 30
   fi
 }
+
+function join_if_exist() {
+    if [ -n "$2" ]; then
+        echo "$1$2"
+    else
+        echo ""
+    fi
+}
+
 #===========================================================================================
 # JVM Configuration
 #===========================================================================================
-if [[ "${MODE}" == "standalone" ]]; then
+Xms=$(join_if_exist "-Xms" ${JVM_XMS})
+Xmx=$(join_if_exist "-Xmx" ${JVM_XMX})
+Xmn=$(join_if_exist "-Xmn" ${JVM_XMN})
+XX_MS=$(join_if_exist "-XX:MetaspaceSize=" ${JVM_MS})
+XX_MMS=$(join_if_exist "-XX:MaxMetaspaceSize=" ${JVM_MMS})
 
-  JAVA_OPT="${JAVA_OPT} -Xms${JVM_XMS} -Xmx${JVM_XMX} -Xmn${JVM_XMN}"
+if [[ "${MODE}" == "standalone" ]]; then
+  JAVA_OPT="${JAVA_OPT} $Xms $Xmx $Xmn"
   JAVA_OPT="${JAVA_OPT} -Dnacos.standalone=true"
 else
   if [[ "${EMBEDDED_STORAGE}" == "embedded" ]]; then
     JAVA_OPT="${JAVA_OPT} -DembeddedStorage=true"
   fi
-  JAVA_OPT="${JAVA_OPT} -server -Xms${JVM_XMS} -Xmx${JVM_XMX} -Xmn${JVM_XMN} -XX:MetaspaceSize=${JVM_MS} -XX:MaxMetaspaceSize=${JVM_MMS}"
+  JAVA_OPT="${JAVA_OPT} -server $Xms $Xmx $Xmn $XX_MS $XX_MMS"
   if [[ "${NACOS_DEBUG}" == "y" ]]; then
-    JAVA_OPT="${JAVA_OPT} -Xdebug -Xrunjdwp:transport=dt_socket,address=9555,server=y,suspend=n"
+    JAVA_OPT="${JAVA_OPT} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:9555"
   fi
   JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${BASE_DIR}/logs/java_heapdump.hprof"
   JAVA_OPT="${JAVA_OPT} -XX:-UseLargePages"
@@ -78,15 +92,43 @@ if [[ ! -z "${NACOS_AUTH_ENABLE}" ]]; then
   JAVA_OPT="${JAVA_OPT} -Dnacos.core.auth.enabled=${NACOS_AUTH_ENABLE}"
 fi
 
+if [[ ! -z "${NACOS_AUTH_ADMIN_ENABLE}" ]]; then
+  JAVA_OPT="${JAVA_OPT} -Dnacos.core.auth.admin.enabled=${NACOS_AUTH_ADMIN_ENABLE}"
+fi
+
+if [[ ! -z "${NACOS_AUTH_CONSOLE_ENABLE}" ]]; then
+  JAVA_OPT="${JAVA_OPT} -Dnacos.core.auth.console.enabled=${NACOS_AUTH_CONSOLE_ENABLE}"
+fi
+
+if [[ -z "${NACOS_AUTH_TOKEN}" ]]; then
+  echo "env NACOS_AUTH_TOKEN must be set with Base64 String."
+  exit 255
+fi
+
+if [[ -z "${NACOS_AUTH_IDENTITY_KEY}" ]]; then
+  echo "env NACOS_AUTH_IDENTITY_KEY must be set."
+  exit 255
+fi
+
+if [[ -z "${NACOS_AUTH_IDENTITY_VALUE}" ]]; then
+  echo "env NACOS_AUTH_IDENTITY_VALUE must be set."
+  exit 255
+fi
+
 if [[ "${PREFER_HOST_MODE}" == "hostname" ]]; then
   JAVA_OPT="${JAVA_OPT} -Dnacos.preferHostnameOverIp=true"
 fi
 JAVA_OPT="${JAVA_OPT} -Dnacos.member.list=${MEMBER_LIST}"
 
+if [[ ! -z "${NACOS_DEPLOYMENT_TYPE}" ]]; then
+  JAVA_OPT="${JAVA_OPT} -Dnacos.deployment.type=${NACOS_DEPLOYMENT_TYPE}"
+fi
+
 JAVA_MAJOR_VERSION=$($JAVA -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
 if [[ "$JAVA_MAJOR_VERSION" -ge "9" ]]; then
   JAVA_OPT="${JAVA_OPT} -Xlog:gc*:file=${BASE_DIR}/logs/nacos_gc.log:time,tags:filecount=10,filesize=102400"
 else
+  JAVA_OPT="${JAVA_OPT} -XX:+UseConcMarkSweepGC -XX:+UseCMSCompactAtFullCollection -XX:CMSInitiatingOccupancyFraction=70 -XX:+CMSParallelRemarkEnabled -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 "
   JAVA_OPT_EXT_FIX="-Djava.ext.dirs=${JAVA_HOME}/jre/lib/ext:${JAVA_HOME}/lib/ext"
   JAVA_OPT="${JAVA_OPT} -Xloggc:${BASE_DIR}/logs/nacos_gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M"
 fi
@@ -98,7 +140,7 @@ JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
 JAVA_OPT="${JAVA_OPT} --spring.config.additional-location=${CUSTOM_SEARCH_LOCATIONS}"
 JAVA_OPT="${JAVA_OPT} --spring.config.name=${CUSTOM_SEARCH_NAMES}"
 JAVA_OPT="${JAVA_OPT} --logging.config=${BASE_DIR}/conf/nacos-logback.xml"
-JAVA_OPT="${JAVA_OPT} --server.max-http-header-size=524288"
+JAVA_OPT="${JAVA_OPT} --server.max-http-request-header-size=524288"
 
 echo "Nacos is starting, you can docker logs your container"
 exec $JAVA ${JAVA_OPT}
